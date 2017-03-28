bd54a0601b212334a2c7907faee7172fd3cf9d99
OctaM <octa.marian@gmail.com>
2017-03-28 19:03:46 +0300

    Added story bar

--- app/NewsFeed/NewsFeed.js
+++ app/NewsFeed/NewsFeed.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text, ScrollView, StyleSheet, Image} from 'react-native';
 import NewsFeedPost from './NewsFeedPost.js';
+import Carousel from 'react-native-snap-carousel'
 
 export default class NewsFeed extends Component {
 
@@ -20,6 +21,7 @@ export default class NewsFeed extends Component {
                             <Image source={require('./Img/photoCamera.png')}/>
                         </View>
                     </View>
+                    <Storyes/>
                     <ScrollView showsVerticalScrollIndicator={false}>
                         <NewsFeedPost/>
                         <NewsFeedPost/>
@@ -31,6 +33,42 @@ export default class NewsFeed extends Component {
             </View>
         )
     }
+}
+
+class Storyes extends Component {
+  render() {
+    return(
+      <View style={style.carouselView}>
+        <Carousel
+          ref = {'carousel'}
+          sliderWidth = {30}
+          itemWidth = {30}
+          snapOnAndroid = {false}
+          showsHorizontalScrollIndicator = {false}
+          showsVerticalScrollIndicator = {false}
+          inactiveSlideScale = {1}
+          bounce = {false}
+          contentContainerStyle = {style.carouselContainer}>
+            <Story/>
+            <Story/>
+            <Story/>
+            <Story/>
+            <Story/>
+            <Story/>
+        </Carousel>
+      </View>
+    )
+  }
+}
+
+class Story extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('./Img/storyPhoto.jpg')}  style = {style.storyPhoto}/>
+      </View>
+    )
+  }
 }
 
 const style = StyleSheet.create({
@@ -65,5 +103,21 @@ const style = StyleSheet.create({
         justifyContent: 'space-between',
         borderBottomWidth: 1,
         borderColor: 'rgba(0, 0, 0, 0.2)'
-    }
+    },
+    storyPhoto: {
+      borderRadius: 50,
+      height: 55,
+      width: 55,
+      marginRight: 20,
+    },
+    carouselView: {
+        flexDirection: 'row',
+        height:100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0,0,0,0.02)',
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(0, 0, 0, 0.03)',
+    },
+
 })
